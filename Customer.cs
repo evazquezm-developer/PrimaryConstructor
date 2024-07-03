2db09b96f3fd9ee1b6d3621ea45ac929cbdddcd9
Elias Vazquez <eliasvm@yahoo.com>
2024-07-03 12:01:59 -0600

    I've added a empty constructor.

--- Customer.cs
+++ Customer.cs
@@ -1,6 +1,11 @@
-// Constructor is declared at class level.
+// Constructor with two parameters is declared at class level.
 
 public class Customer(string firstName, string lastName)
 {
+    // Constructor empty
+    public Customer() : this("", "")
+    {
+    }
+
     public string GetFullName() => $"{firstName} - {lastName}";
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿
 Console.WriteLine("Example using Primary Constructors");
 
+// Constructor with parameters.
 var customer = new Customer("C#", "12");
 
 Console.WriteLine($"{customer.GetFullName()}");
+
+// Constructor empty.
+var newCustomer = new Customer();
+Console.WriteLine($"{newCustomer.GetFullName()}");
